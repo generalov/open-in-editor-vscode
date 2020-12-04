9edb61f49ccbc1db80a8fbe743a49f70246aa889
GrumpyLittleTed <grumpylittleted@googlemail.com>
2020-12-04 19:01:12 +0000

    Fix config not getting loaded - some confusion over config settings and file config passed by editor

--- src/extension.ts
+++ src/extension.ts
@@ -1,142 +1,133 @@
 'use strict';
 import * as vscode from 'vscode';
-import { configure as findEditor } from '@generalov/open-in-editor';
+import {configure as findEditor} from '@generalov/open-in-editor';
 
 const extension = 'alt-editor';
 
 interface EditorConfig {
-    name?: String;
-    binary?: String;
-    args?: String;
-    terminal?: Boolean;
+  name?: String;
+  binary?: String;
+  args?: String;
+  terminal?: Boolean;
 }
 
 interface EditorOptions {
-    editor?: String;
-    cmd?: String;
-    pattern?: String;
-    terminal?: Boolean;
+  editor?: String;
+  cmd?: String;
+  pattern?: String;
+  terminal?: Boolean;
 }
 
 interface Editor {
-    open(path: String): Promise<any>;
+  open(path: String): Promise<any>;
 }
 
 function getEditorOptions(config: EditorConfig): EditorOptions {
-    const editorOptions: EditorOptions  = {};
+  const editorOptions: EditorOptions = {};
 
-    if (config.name != null) {
-        editorOptions.editor = String(config.name);
-    }
-    if (config.binary != null) {
-        editorOptions.cmd = String(config.binary);
-    }
-    if (config.args != null) {
-        editorOptions.pattern = String(config.args);
-    }
-    if (config.terminal != null) {
-        editorOptions.terminal = Boolean(config.terminal);
-    }
+  if (config.name != null) {
+    editorOptions.editor = String(config.name);
+  }
+  if (config.binary != null) {
+    editorOptions.cmd = String(config.binary);
+  }
+  if (config.args != null) {
+    editorOptions.pattern = String(config.args);
+  }
+  if (config.terminal != null) {
+    editorOptions.terminal = Boolean(config.terminal);
+  }
 
-    return editorOptions;
+  return editorOptions;
 }
 
 function createEditor(editorOptions: EditorOptions) {
-    return new Promise((resolve, reject) => {
-        if (!Object.keys(editorOptions)) {
-            reject(new Error(`No editor configuration found. Please check the "${extension}" settings.`));
-            return;
-        }
+  return new Promise((resolve, reject) => {
+    if (!Object.keys(editorOptions)) {
+      reject(new Error(`No editor configuration found. Please check the "${extension}" settings.`));
+      return;
+    }
 
-        const editor = findEditor(editorOptions);
+    const editor = findEditor(editorOptions);
 
-        if (!editor) {
-            reject(new Error(`External editor not found. Please check the "${extension}" settings.`));
-            return;
-        } else {
-            resolve(editor);
-            return;
-        }
-    });
+    if (!editor) {
+      reject(new Error(`External editor not found. Please check the "${extension}" settings.`));
+      return;
+    } else {
+      resolve(editor);
+      return;
+    }
+  });
 }
 
-function openFile(fileName: String, line: Number = 1, column: Number = 1, config: EditorConfig={}) {
-    if (!config || Object.keys(config).length === 0) {
-        config = <EditorConfig>vscode.workspace.getConfiguration(extension);
-    }
-    const options = getEditorOptions(config);
-    const fileLoc = `${fileName}:${line}:${column}`;
-    const intro = vscode.window.setStatusBarMessage(`Opening ${fileLoc}...`);
-    const res = createEditor(options)
-        .then((editor: Editor) => editor.open(fileLoc))
-        .then((openResult: any) => {
-            intro.dispose();
-            vscode.window.setStatusBarMessage(`Opened ${fileLoc}`, 5000);
+function openFile(fileName: String, line: Number = 1, column: Number = 1, config: EditorConfig = {}) {
+  config = <EditorConfig>vscode.workspace.getConfiguration(extension);
 
-            return openResult;
-        })
-        .catch((err) => {
-            vscode.window.showErrorMessage(`Can't open the file ${fileLoc}: ${err}`)
-                .then(() => intro.dispose());
-        });
+  const options = getEditorOptions(config);
+  const fileLoc = `${fileName}:${line}:${column}`;
+  const intro = vscode.window.setStatusBarMessage(`Opening ${fileLoc}...`);
+  const res = createEditor(options)
+    .then((editor: Editor) => editor.open(fileLoc))
+    .then((openResult: any) => {
+      intro.dispose();
+      vscode.window.setStatusBarMessage(`Opened ${fileLoc}`, 5000);
 
-    return res;
+      return openResult;
+    })
+    .catch(err => {
+      vscode.window.showErrorMessage(`Can't open the file ${fileLoc}: ${err}`).then(() => intro.dispose());
+    });
+
+  return res;
 }
 
 function openTextEditorFile(textEditor: vscode.TextEditor, config: EditorConfig) {
-    if (textEditor.document.isUntitled) {
-        vscode.window.showErrorMessage('Please save the file first.');
-        return;
-    }
+  if (textEditor.document.isUntitled) {
+    vscode.window.showErrorMessage('Please save the file first.');
+    return;
+  }
 
-    const position = textEditor.selection.active;
+  const position = textEditor.selection.active;
 
-    return openFile(textEditor.document.fileName,
-             position.line + 1,
-             position.character + 1,
-             config);
+  return openFile(textEditor.document.fileName, position.line + 1, position.character + 1, config);
 }
 
-
 function openFileMenu(uri: vscode.Uri) {
-    if (!uri || uri.scheme !== 'file' || !uri.fsPath) {
-        vscode.window.showErrorMessage('Please select a local file.');
-        return;
-    }
+  if (!uri || uri.scheme !== 'file' || !uri.fsPath) {
+    vscode.window.showErrorMessage('Please select a local file.');
+    return;
+  }
 
-    return openFile(uri.fsPath);
+  return openFile(uri.fsPath);
 }
 
 function openFileCommand(...args: any[]) {
-    args = args.filter(Boolean)
-    let uri: vscode.Uri = null;
-    let editor: vscode.TextEditor = vscode.window.activeTextEditor;
-    let config: EditorConfig = args.length === 2 ? args[1] : null;
+  args = args.filter(Boolean);
+  let uri: vscode.Uri = null;
+  let editor: vscode.TextEditor = vscode.window.activeTextEditor;
+  let config: EditorConfig = args.length === 2 ? args[1] : null;
 
-    if (args.length) {
-        if (args[0].scheme) {
-            if (!vscode.window.activeTextEditor ||
-                args[0].path !== vscode.window.activeTextEditor.document.fileName
-            ) {
-                uri = args[0]
-            }
-        } else if (args[0].document) {
-            editor = args[0]
-        } else {
-            config = args[0]
-        }
-    }
-
-    if (uri) {
-        openFileMenu(uri)
+  if (args.length) {
+    if (args[0].scheme) {
+      if (!vscode.window.activeTextEditor || args[0].path !== vscode.window.activeTextEditor.document.fileName) {
+        uri = args[0];
+      }
+    } else if (args[0].document) {
+      editor = args[0];
     } else {
-        openTextEditorFile(editor, config);
-    } 
+      config = args[0];
+    }
+  }
+
+  if (uri) {
+    openFileMenu(uri);
+  } else {
+    openTextEditorFile(editor, config);
+  }
 }
 
 export function activate(context: vscode.ExtensionContext) {
-    context.subscriptions.push(vscode.commands.registerCommand(`${extension}.openFile`, openFileCommand));
+  context.subscriptions.push(vscode.commands.registerCommand(`${extension}.openFile`, openFileCommand));
 }
 
-export function deactivate() {
-}
+export function deactivate() {}
